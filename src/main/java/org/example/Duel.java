52899c1f79684c857070b8d18c44f3a5f1351370
Ceyda Oymak <ceydaoymak09@gmail.com>
2025-04-22 17:58:54 +0300

    Add Smoke Tests for Week 5 system (v2 for Week7)

--- src/main/java/org/example/Duel.java
+++ src/main/java/org/example/Duel.java
@@ -1,25 +1,29 @@
 package org.example;
+
+import java.util.Random;
 /**
  * @requires s1 != null && s2 != null && spell != null
  * @effects Simulates duel and prints result
  */
-public class Duel {
+class Duel {
     public static void start(HogwartsStudent s1, HogwartsStudent s2, String spell) {
+        if (s1 == null || s2 == null || spell == null) {
+            throw new NullPointerException("Student or spell cannot be null");
+        }
+
+        System.out.println("Duel between " + s1.getName() + " and " + s2.getName() + " using " + spell);
         boolean s1Knows = s1.knowsSpell(spell);
         boolean s2Knows = s2.knowsSpell(spell);
 
         if (!s1Knows && !s2Knows) {
-            System.out.println(" None of the students knows the spell.");
+            System.out.println("Draw! Neither student knows the spell.");
         } else if (s1Knows && !s2Knows) {
-            System.out.println(s1.getName() + "wins");
+            System.out.println(s1.getName() + " wins!");
         } else if (!s1Knows && s2Knows) {
-            System.out.println(s2.getName() + "wins");
+            System.out.println(s2.getName() + " wins!");
         } else {
-            if (Math.random() < 0.5) {
-                System.out.println(s1.getName() + "wins");
-            } else {
-                System.out.println(s2.getName() + "wins");
-            }
+            int winner = new Random().nextInt(2);
+            System.out.println((winner == 0 ? s1.getName() : s2.getName()) + " wins the duel by magic power!");
         }
     }
 }
